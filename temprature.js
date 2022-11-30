212959cde0dba62dc53386afd997665b4fb7288f
maazahmar <maazahmar@gmail.com>
2022-11-30 11:38:05 +0545

    completed

--- temprature.js
+++ temprature.js
@@ -2,7 +2,16 @@ var data = fetch('https://api.openweathermap.org/data/2.5/weather?q=biratnagar&a
 data.then((value)=>value.json()).then((res)=>{
     var temprature = res['main']['temp'];
     var icon = res['weather'][0]['icon'];
+    icon = icon.toString();
+    if(icon.includes('d')){
+        
+        icon  = icon.replace('d','n'); 
+    }
     document.getElementById('temp').innerHTML = conversonal(temprature) + '°C';
+    document.getElementById('icon').setAttribute('src','https://openweathermap.org/img/wn/'+icon + '.png')
+    console.log(document.getElementById('icon').getAttribute('src'));
+    document.getElementById('icon').setAttribute('width','35');
+    document.getElementById('icon').setAttribute('filter','grayscale(100%)');
 })
 
 function conversonal(val){
